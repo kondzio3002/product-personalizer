62f38e561e27ba396c86c3e8e6d4727433f1bcd4
Konrad Brożyna <kondzio3002@gmail.com>
2022-11-23 21:37:26 +0100

    Finish edit Product

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -7,24 +7,8 @@ const Products = () => {
 
   return (
     <section>
-      {products.map(product => <Product {...products[product.id - "1"]} />)}
+      {products.map(product => <Product key={product.id} {...product} />)}
     </section>
-    /* <section>
-       <Product
-        id={products[0].id}
-        name={products[0].name}
-        title={products[0].title}
-        colors={products[0].colors}
-        sizes={products[0].sizes}
-        basePrice={products[0].basePrice} />
-      <Product
-        id={products[1].id}
-        name={products[1].name}
-        title={products[1].title}
-        colors={products[1].colors}
-        sizes={products[1].sizes}
-        basePrice={products[1].basePrice} />
-    </section> */
   );
 };
 
